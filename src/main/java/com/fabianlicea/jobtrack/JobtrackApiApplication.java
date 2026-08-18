c8aba3150ccb0517449d1ce15a3e3833ca998eb8
FabianLIC <fabianlicea50@gmail.com>
2026-08-18 18:26:53 +0200

    feat: primer endpoint GET /api/candidaturas

--- src/main/java/com/fabianlicea/jobtrack/JobtrackApiApplication.java
+++ src/main/java/com/fabianlicea/jobtrack/JobtrackApiApplication.java
@@ -2,12 +2,14 @@ package com.fabianlicea.jobtrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class JobtrackApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobtrackApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobtrackApiApplication.class, args);
+    }
 
 }
